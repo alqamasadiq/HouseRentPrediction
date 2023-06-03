c56939de36e304b7aee2beb239f5d5288041e7ba
alqamasadiq <54991785+alqamasadiq@users.noreply.github.com>
2023-06-03 21:20:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request,render_template
+import pickle
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('HouseRentPridiction_2.html')
+
+# prediction function 
+def ValuePredictor(to_predict_list): 
+    to_predict = np.array(to_predict_list).reshape(1, 8) 
+    loaded_model = pickle.load(open("HouseRent.pkl", "rb")) 
+    result = loaded_model.predict(to_predict) 
+    return result[0] 
+  
+@app.route('/result', methods = ['POST']) 
+def result(): 
+    if request.method == 'POST':
+        #required_fields = ['bhk', 'flatsize', 'areatype','city','fs','bathroom','pc','bf']
+        required_fields = {'City':'city', 'BHK':'bhk', 'Furniture Status':'furstatus','Bathroom':'bathroom','Area Type':'areatype','Point of Contact':'pointofcontact','Flat Size':'flatsize','Building Floor':'buildingfloor'}
+        errors = []
+        for key,value in required_fields.items():
+            if value not in request.form or not request.form[value]:
+                errors.append(f"{Key} is required.")
+        if errors:
+            return render_template('result.html', prediction = errors)
+        to_dictionary = request.form.to_dict() 
+        to_predict_list = request.form.to_dict() 
+        to_predict_list = list(to_predict_list.values()) 
+        to_predict_list = list(map(int, to_predict_list)) 
+        result = ValuePredictor(to_predict_list)                     
+        return render_template("result.html", prediction = round(result,2)) 
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
